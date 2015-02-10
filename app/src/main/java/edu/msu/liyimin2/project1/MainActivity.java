cde5c63c64023b562a2b007ebd7410680bf6783f
Justin Rucinski <rucins11@cse.msu.edu>
2015-02-10 17:48:24 -0500

    Fixed keyboard

--- app/src/main/java/edu/msu/liyimin2/project1/MainActivity.java
+++ app/src/main/java/edu/msu/liyimin2/project1/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,7 +15,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
 
