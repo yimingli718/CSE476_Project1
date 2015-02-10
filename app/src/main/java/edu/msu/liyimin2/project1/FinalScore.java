7a31f34258098a0e4cbbd7f6a440c2a8070f79d7
Justin Rucinski <rucins11@cse.msu.edu>
2015-02-10 17:33:22 -0500

    Minor Updates

--- app/src/main/java/edu/msu/liyimin2/project1/FinalScore.java
+++ app/src/main/java/edu/msu/liyimin2/project1/FinalScore.java
@@ -1,9 +1,11 @@
 package edu.msu.liyimin2.project1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class FinalScore extends ActionBarActivity {
@@ -35,5 +37,11 @@ public class FinalScore extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onMenu(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
