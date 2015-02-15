7c60ed128562c3547972c17c66a8ce541a44a0f6
Justin Rucinski <rucins11@cse.msu.edu>
2015-02-15 15:03:31 -0500

    Game stuff added to main activity

--- app/src/main/java/edu/msu/liyimin2/project1/Game.java
+++ app/src/main/java/edu/msu/liyimin2/project1/Game.java
@@ -4,6 +4,8 @@ package edu.msu.liyimin2.project1;
  * Created by Justin on 2/15/2015.
  */
 public class Game {
+    //region "Member variables"
+
     /**
      * Player 1
      */
@@ -14,4 +16,17 @@ public class Game {
      * Player 2
      */
     public Player player2;
+
+    //endregion
+
+    //region "Constructor"
+
+    public Game(){
+        player1 = new Player();
+        player2 = new Player();
+    }
+
+    //endregion
+
+    //
 }
--- app/src/main/java/edu/msu/liyimin2/project1/Player.java
+++ app/src/main/java/edu/msu/liyimin2/project1/Player.java
@@ -3,6 +3,8 @@ package edu.msu.liyimin2.project1;
 import java.util.ArrayList;
 
 public class Player {
+    //region "Member variables"
+
     /**
      * Player name
      */
@@ -18,4 +20,14 @@ public class Player {
      */
     public ArrayList<Bird> birdList = new ArrayList<Bird>();
 
+    //endregion
+
+    //region "Constructor"
+
+    public Player()
+    {
+        score = 0;
+    }
+
+    //endregion
 }
