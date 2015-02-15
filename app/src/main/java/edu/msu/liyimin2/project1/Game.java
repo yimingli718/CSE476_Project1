74fe434b146b30f253a2da47e88014424e132740
Yiming Li <liyimin2@cse.msu.edu>
2015-02-15 15:36:43 -0500

    add draw function to the game function

--- app/src/main/java/edu/msu/liyimin2/project1/Game.java
+++ app/src/main/java/edu/msu/liyimin2/project1/Game.java
@@ -1,5 +1,9 @@
 package edu.msu.liyimin2.project1;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 /**
  * Created by Justin on 2/15/2015.
  */
@@ -13,4 +17,41 @@ public class Game {
      * Player 2
      */
     public Player player2;
+
+    /**
+     * Percentage of the display width or height that
+     * is occupied by the puzzle.
+     */
+    final static float SCALE_IN_VIEW = 0.9f;
+    /**
+     * Paint for filling the area the puzzle is in
+     */
+    private Paint fillPaint;
+
+    /**
+     * Paint for outlining the area the puzzle is in
+     */
+    private Paint outlinePaint;
+
+    /*public Game(){
+
+    }
+
+    public Game(Context context){
+
+    }
+
+    public void draw(Canvas canvas){int wid = canvas.getWidth();
+        int hit = canvas.getHeight();
+
+        // Determine the minimum of the two dimensions
+        int minDim = wid < hit ? wid : hit;
+        int puzzleSize = (int)(minDim * SCALE_IN_VIEW);
+
+        // Compute the margins so we center the puzzle
+        int marginX = (wid - puzzleSize) / 2;
+        int marginY = (hit - puzzleSize) / 2;
+
+        canvas.drawRect(marginX, marginY, marginX + puzzleSize, marginY + puzzleSize, fillPaint);
+    }*/
 }
