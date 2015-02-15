b1265744f5d7d9a13b29bc4dc8e09cc3a634518a
Yiming Li <liyimin2@cse.msu.edu>
2015-02-15 14:28:17 -0500

    add GameView class

---
+++ app/src/main/java/edu/msu/liyimin2/project1/GameView.java
@@ -0,0 +1,43 @@
+package edu.msu.liyimin2.project1;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
